fc9be3d9e3ded17db55a4bcbb2a3aea4fc0c27df
Edmondas Girkantas <eg@fbsd.lt>
2012-06-15 17:38:04 +0300

    added method which returns array with database settings

--- vendor/Poweradmin/Config.php
+++ vendor/Poweradmin/Config.php
@@ -12,13 +12,28 @@ class Config
 
         // load user defined setting
         if (file_exists('inc/config.inc.php')) {
-            // TODO: what if there is syntax error?
             include('inc/config.inc.php');
         } else {
-            // TODO: display error
-            die( _('You have to create a config.inc.php!') );
+            die(_('You have to create a config.inc.php!'));
         }
 
+        // put all configuration settings to array
         $this->items = compact(array_keys(get_defined_vars()));
+    }
+
+    function getDbSettings() {
+        $settings = array();
+
+        $settings['hostname'] = (isset($this->items['db_host']) ? $this->items['db_host'] : null);
+        $settings['port'] = (isset($this->items['db_port']) ? $this->items['db_port'] : null);
+        $settings['username'] = (isset($this->items['db_user']) ? $this->items['db_user'] : null);
+        $settings['password'] = (isset($this->items['db_pass']) ? $this->items['db_pass'] : null);
+        $settings['database'] = (isset($this->items['db_name']) ? $this->items['db_name'] : null);
+        $settings['type'] = (isset($this->items['db_type']) ? $this->items['db_type'] : null);
+        $settings['file'] = (isset($this->items['db_file']) ? $this->items['db_file'] : null);
+        $settings['debug'] = (isset($this->items['db_debug']) ? $this->items['db_debug'] : null);
+        $settings['layer'] = (isset($this->items['db_layer']) ? $this->items['db_layer'] : null);
+
+        return $settings;
     }
 }
