b52e0ae91c618e5b369f81cd8c046b8ac5909645
Edmondas Girkantas <eg@fbsd.lt>
2012-06-15 17:39:20 +0300

    added dummy Database class

---
+++ vendor/Poweradmin/Database.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Poweradmin;
+
+class Database
+{
+    // TODO: reimplement stuff from inc/database.inc.php
+
+    function __construct($settings) {
+        // TODO: check db settings
+    }
+
+}
