81031e969245865dcfce742c879a704705d310e2
Edmondas Girkantas <eg@fbsd.lt>
2012-06-16 08:43:18 +0300

    added autoloading of PDO library if db layer is not defined, otherwise use old MDB2

--- vendor/Poweradmin/Database.php
+++ vendor/Poweradmin/Database.php
@@ -4,10 +4,32 @@ namespace Poweradmin;
 
 class Database
 {
-    // TODO: reimplement stuff from inc/database.inc.php
+    // TODO: reimplement code from inc/database.inc.php
 
     function __construct($settings) {
         // TODO: check db settings
+
+        /*
+         * If DB layer is not defined in configuration file, then use PDO by default,
+         * because MDB2 library is unmaintained for 2 years and also it uses huge
+         * amount of memory compared to PDO
+         */
+        if (!$settings['layer']) {
+            $settings['layer'] = 'PDO';
+        }
+
+        if ($settings['layer'] == 'MDB2') {
+            (@include 'MDB2.php') or die ('You have to install MDB2 library!');
+        } else {
+            if (class_exists('PDO', false)) {
+                include 'inc/PDOLayer.php';
+            } else {
+                die ('You have to install PDO library!');
+            }
+        }
+
+        // TODO: build dsn
+        // TODO: connect to db
     }
 
 }
